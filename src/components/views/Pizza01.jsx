ab176d7ff6a20fedbf3c2133ca7b4464a1948e96
Tomas <tomascorvalan14@gmail.com>
2024-09-15 20:47:02 -0300

    se soluciona error en pizza01 que no dejaba renderizar pizzas

--- src/components/views/Pizza01.jsx
+++ src/components/views/Pizza01.jsx
@@ -1,25 +1,26 @@
-import React, { useEffect } from "react";
+import { useContext, useEffect } from "react";
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
 import Button from "react-bootstrap/Button";
 import Pizzas from "../json/pizzas";
 import { useNavigate, useParams } from "react-router-dom";
+import { CartContext } from "../Context/CartContext";
 
 const Pizza01 = () => {
   const { id } = useParams();
   const navigate = useNavigate();
-  const pizza = Pizzas.find((p) => p.id === parseInt(id));
-
+  const pizza = Pizzas.find((p) => p.id === id); //el id es string, si fuese numerico es parseInt
+  const { catchPizza } = useContext(CartContext);
   useEffect(() => {
     if (!pizza) {
-      navigate("/HITO7_REACT/");
+      navigate("/HITO7_REACT/*");
     }
   }, [pizza, navigate]);
 
   return (
     <div className="pizzacontainer d-flex justify-content-center align-items-center">
       <Card style={{ width: "60vh" }} className="pizza" key={pizza.id}>
-        <Card.Img variant="top" src={pizza.img} />
+        <Card.Img variant="top" src={pizza.img} alt="imagen pizza" />
         <Card.Body>
           <Card.Title className="pizza_name">{pizza.name}</Card.Title>
           <div className="ingredientes">
